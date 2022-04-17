ad26cf74e17f8c9cf986e657920e35feb2a141fd
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2022-04-17 07:49:04 -0400

    a temp file

---
+++ gtk-3.0/colors-change-deciding.sh
@@ -0,0 +1,78 @@
+##### purples (to sort)
+f7edf2/@lavenderblush-f7edf2
+5f3a4b/@purple-5f3a4b
+724157/@purple-724157
+642c2c/@maroon-642c2c
+593737/@maroon-593737
+
+##### whites (to sort)
+e0b8ca/@thistle-e0b8ca
+f6eded/@snow-f6eded
+d7dae3/@gainsboro-d7dae3
+fbe4bc/@wheat-fbe4bc
+FFFFBF/@lemonchiffon-FFFFBF
+
+##### oranges (to sort)
+ff8d14/@darkorange-ff8d14
+ff8f1a/@darkorange-ff8f1a
+FF8300/@darkorange-FF8300
+ec7c06/@darkorange-ec7c06
+
+##### blacks (to sort)
+1a1a1a/@black-1a1a1a
+
+##### browns (to sort)
+7a4e1f/@saddlebrown-7a4e1f
+7e4242/@brown-7e4242
+884444/@brown-884444
+964242/@brown-964242
+9c4848/@brown-9c4848
+A74949/@brown-A74949
+926a46/@sienna-926a46
+b86814/@chocolate-b86814
+c86f10/@chocolate-c86f10
+A77549/@peru-A77549
+ca9292/@rosybrown-ca9292
+
+##### reds (to sort)
+a9696e/@indianred-a9696e
+b74d4d/@indianred-b74d4d
+b94f4f/@indianred-b94f4f
+b96d6d/@indianred-b96d6d
+dd88ae/@palevioletred-dd88ae
+
+##### blue (to sort)
+0055FF/@royalblue-0055FF
+0653ec/@royalblue-0653ec
+104ec8/@royalblue-104ec8
+1463ff/@royalblue-1463ff
+1a66ff/@royalblue-1a66ff
+495DA7/@royalblue-495DA7
+4f66b9/@royalblue-4f66b9
+4992A7/@cadetblue-4992A7
+468192/@teal-468192
+
+##### grays (to sort)
+575962/@dimgray-575962
+62646d/@dimgray-62646d
+656979/@dimgray-656979
+686a75/@dimgray-686a75
+6f717d/@dimgray-6f717d
+737373/@dimgray-737373
+838795/@lightslategray-838795
+878ca0/@lightslategray-878ca0
+898d9a/@lightslategray-898d9a
+8b8e9c/@lightslategray-8b8e9c
+9196a9/@lightslategray-9196a9
+9397a8/@lightslategray-9397a8
+979aa7/@darkgray-979aa7
+999999/@darkgray-999999
+9a9daa/@darkgray-9a9daa
+9a9fb2/@darkgray-9a9fb2
+abacb1/@darkgray-abacb1
+b3b3b3/@darkgray-b3b3b3
+a4a8bb/@darkgray-a4a8bb
+a5a9b9/@darkgray-a5a9b9
+9da0b0/@darkgray-9da0b0
+bfbfbf/@silver-bfbfbf
+c2c6d3/@silver-c2c6d3
